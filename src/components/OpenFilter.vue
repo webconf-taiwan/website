a667f9e8c91fedfc5d9e2b3d33bd04b4fb7fb38f
NOICHAN <85007358+NOICHAN@users.noreply.github.com>
2023-06-29 00:39:19 +0800

    Change button name (#30)
    * 將即將開賣文字改成前往購票
    
    * 更改 MoveToTop 名稱
    
    * 刪除 console
    
    * 刪除 console part2

---
+++ src/components/OpenFilter.vue
@@ -0,0 +1,30 @@
+<template>
+  <a href="#" @click.prevent="openFilter">
+    <div
+      class="relative flex flex-col items-center justify-center w-10 h-10 border border-custom-teal-500 bg-custom-gray-800"
+    >
+      <div class="w-9 h-9 bg-cover filterIcon"></div>
+    </div>
+  </a>
+</template>
+
+<style scoped>
+.filterIcon {
+  background-image: url("@/assets/images/icon/ic_filter_l.svg");
+}
+</style>
+
+<script setup>
+import { onMounted } from "vue";
+import { useFilterStore } from "@/stores/filter";
+
+const filterStore = useFilterStore();
+const { openFilter } = filterStore;
+
+onMounted(() => {
+  const viewportWidth = window.innerWidth;
+  if (viewportWidth >= 768) {
+    openFilter();
+  }
+});
+</script>
