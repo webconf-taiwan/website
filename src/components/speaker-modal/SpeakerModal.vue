73f324aeb002fc27c984fa589c2fd9614022531a
Alex Lau <alex.wanna.nirvana@gmail.com>
2023-07-01 01:54:18 +0800

    feat: 講者頁面根據 json 動態渲染, modal 按下 esc 可關閉

--- src/components/speaker-modal/SpeakerModal.vue
+++ src/components/speaker-modal/SpeakerModal.vue
@@ -7,7 +7,7 @@
       class="relative border-custom-teal-700 border-2 rounded-sm bg-black pb-8 pt-12 px-4 md:px-5 text-white w-[792px] scroll overflow-y-auto max-h-full"
     >
       <button
-        @click="closeModal"
+        @click="onModalClose"
         class="absolute top-3 right-5 md:right-7 md:top-5 stroke-current text-custom-teal-500"
       >
         <iconClose class="hover:text-custom-teal-700 active:text-custom-teal-500 w-6 h-6" />
@@ -15,40 +15,40 @@
       <!-- 講者頭貼 -->
       <div class="flex md:px-3 mb-9 md:mb-5 flex-col md:flex-row items-center md:items-stretch">
         <img
-          class="mb-5 md:mb-0 mx-auto w-full max-w-[360px] max-h-[360px] md:w-[150px] md:h-[150px] border-2 border-custom-teal-500 md:mr-5 md:ml-0 object-contain"
-          :src="props.speakerInfo.photoURL"
+          class="mb-5 md:mb-0 mx-auto w-full max-w-[360px] max-h-[360px] md:w-[150px] md:h-[150px] border-2 border-custom-teal-500 md:mr-5 md:ml-0 object-cover"
+          :src="speakerInfo.photoURL"
         />
         <div class="flex flex-col">
           <h2
             class="text-custom-teal-500 font-semibold text-2xl md:text-3xl mb-3 text-center md:text-left"
           >
-            {{ props.speakerInfo.speakerName }}
+            {{ speakerInfo.speakerName }}
           </h2>
           <p class="mb-5 md:mb-auto">
-            {{ `${props.speakerInfo.organization} ${props.speakerInfo.jobTitle}` }}
+            {{ `${speakerInfo.organization} ${speakerInfo.jobTitle}` }}
           </p>
 
           <div
             class="flex text-custom-teal-500 justify-center md:justify-start gap-3 md:gap-2 -ml-1"
           >
             <a
-              :href="props.speakerInfo.facebookProfileLink"
-              v-show="props.speakerInfo.facebookProfileLink"
+              :href="speakerInfo.facebookProfileLink"
+              v-show="speakerInfo.facebookProfileLink"
               class="hover:text-custom-teal-700 active:text-custom-teal-500"
             >
               <iconFacebook class="stroke-current md:w-6 md:h-6" />
             </a>
 
             <a
-              :href="props.speakerInfo.twitterProfileLink"
-              v-show="props.speakerInfo.twitterProfileLink"
+              :href="speakerInfo.twitterProfileLink"
+              v-show="speakerInfo.twitterProfileLink"
               class="hover:text-custom-teal-700 active:text-custom-teal-500"
             >
               <iconTwitter class="svg-fill-current md:w-6 md:h-6" />
             </a>
             <a
-              :href="props.speakerInfo.otherLinks"
-              v-show="props.speakerInfo.otherLinks"
+              :href="speakerInfo.otherLinks"
+              v-show="speakerInfo.otherLinks && speakerInfo.otherLinks !== ''"
               class="hover:text-custom-teal-700 active:text-custom-teal-500"
             >
               <iconLink class="svg-fill-current md:w-5 md:h-5" />
@@ -62,7 +62,7 @@
         <h3 class="font-semibold text-lg md:text-xl text-custom-teal-500 leading-tight mb-2">
           講者介紹
         </h3>
-        <p class="leading-normal">{{ props.speakerInfo.personalIntroduction }}</p>
+        <p class="leading-normal whitespace-pre-line">{{ speakerInfo.personalIntroduction }}</p>
       </div>
 
       <!-- 議程資訊 -->
@@ -71,30 +71,35 @@
         class="cursor-pointer flex items-center justify-start border-b border-custom-teal-500 md:px-3 py-3 text-custom-teal-500"
       >
         <h3 class="text-lg md:text-xl font-semibold leading-tight mr-2">議程資訊</h3>
-        <iconArrowUp v-show="isMoreInfoOpen" class="stroke-current" />
-        <iconArrowDown v-show="!isMoreInfoOpen" class="stroke-current" />
+        <iconArrowUp v-if="isMoreInfoOpen" class="stroke-current" />
+        <iconArrowDown v-if="!isMoreInfoOpen" class="stroke-current" />
       </div>
       <Transition name="slide-fade">
         <section class="md:px-3 pt-6" v-show="isMoreInfoOpen">
           <h1 class="text-custom-teal-500 text-xl md:text-3xl font-semibold mb-2">
-            {{ props.speakerInfo.speechTopic }}
+            {{ speakerInfo.speechTopic }}
           </h1>
-          <div class="flex text-custom-pink-700 items-center justify-start leading-none mb-7">
+          <div
+            class="flex text-custom-pink-700 items-center justify-start leading-none mb-7"
+            v-show="speakerInfo.date"
+          >
             <iconTime class="svg-fill-current stroke-0 mr-1" />
-            <p class="mr-5 pt-[1px]">{{ props.speakerInfo.date }} {{ props.speakerInfo.time }}</p>
+            <p class="mr-5 pt-[1px]">{{ speakerInfo.date }} {{ speakerInfo.time }}</p>
             <iconLocation class="stroke-current mr-1" />
-            <p class="align-middle pt-[3px]">講廳 {{ props.speakerInfo.location }}</p>
+            <p class="align-middle pt-[3px]">講廳 {{ speakerInfo.location }}</p>
           </div>
           <div class="mb-5">
-            <p class="mb-3">{{ props.speakerInfo.speechSummary }}</p>
-            <CategoryTag :tag="tag" v-for="tag in props.speakerInfo.categoryTags" :key="tag" />
+            <p class="mb-3 whitespace-pre-line">{{ speakerInfo.speechSummary }}</p>
+            <div class="flex gap-3">
+              <CategoryTag :tag="tag" v-for="tag in speakerInfo.categoryTags" :key="tag" />
+            </div>
           </div>
           <div class="mb-5">
             <h3 class="font-semibold text-lg md:text-xl text-custom-teal-500 leading-tight mb-2">
               目標會眾
             </h3>
-            <p>
-              {{ props.speakerInfo.targetAudience }}
+            <p class="whitespace-pre-line">
+              {{ speakerInfo.targetAudience }}
             </p>
           </div>
 
@@ -102,12 +107,12 @@
             <h3 class="font-semibold text-lg md:text-xl text-custom-teal-500 leading-tight mb-2">
               預期收穫
             </h3>
-            <p>
-              {{ props.speakerInfo.expectedBenefits }}
+            <p class="whitespace-pre-line">
+              {{ speakerInfo.expectedBenefits }}
             </p>
           </div>
 
-          <div class="flex flex-col-reverse md:flex-row gap-3">
+          <!-- <div class="flex flex-col-reverse md:flex-row gap-3">
             <button class="primary-button">
               <iconShare />
               <p>分享議程</p>
@@ -120,7 +125,7 @@
               <iconDate class="stroke-current" />
               <p>加入行事曆</p>
             </button>
-          </div>
+          </div> -->
         </section>
       </Transition>
     </section>
@@ -128,7 +133,8 @@
 </template>
 
 <script setup>
-import { ref, defineProps } from "vue";
+import { ref, defineProps, toRefs, watch, onBeforeUnmount, onMounted } from "vue";
+
 import CategoryTag from "@/components/CategoryTag.vue";
 
 import iconTime from "@/assets/images/icon/ic_time_s.svg";
@@ -139,14 +145,21 @@ import iconArrowUp from "@/assets/images/icon/ic_arrow_up_s.svg";
 import iconFacebook from "@/assets/images/icon/ic_fb_l.svg";
 import iconTwitter from "@/assets/images/icon/ic_twitter_l.svg";
 import iconLink from "@/assets/images/icon/ic_web_l.svg";
-import iconShare from "@/assets/images/icon/ic_share_s.svg";
-import iconDate from "@/assets/images/icon/ic_date_s.svg";
-import iconNote from "@/assets/images/icon/ic_note_s.svg";
+// import iconShare from "@/assets/images/icon/ic_share_s.svg";
+// import iconDate from "@/assets/images/icon/ic_date_s.svg";
+// import iconNote from "@/assets/images/icon/ic_note_s.svg";
 
 const props = defineProps({
   isMoreInfoOpen: {
     default: false,
     type: Boolean,
+  },
+  isModalOpen: {
+    default: false,
+    type: Boolean,
+  },
+  onModalClose: {
+    type: Function,
   },
   speakerInfo: {
     speakerName: String,
@@ -168,16 +181,34 @@ const props = defineProps({
   },
 });
 
-const isModalOpen = ref(true);
 const isMoreInfoOpen = ref(props.isMoreInfoOpen);
 
-const closeModal = () => {
-  isModalOpen.value = false;
-};
+const { onModalClose, speakerInfo, isModalOpen } = toRefs(props);
 
 const toggleMoreInfo = () => {
   isMoreInfoOpen.value = !isMoreInfoOpen.value;
 };
+watch(isModalOpen, (newValue) => {
+  if (typeof window !== "undefined" && window.document) {
+    if (newValue) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }
+});
+const onKeydown = (e) => {
+  if (e.key === "Escape" && isModalOpen.value) {
+    onModalClose.value();
+  }
+};
+onMounted(() => {
+  window.addEventListener("keydown", onKeydown);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("keydown", onKeydown);
+});
 </script>
 
 <style>
--- src/views/SpeakerModalDemoView.vue
+++ src/views/SpeakerModalDemoView.vue
@@ -14,7 +14,7 @@ const speakerInfo = {
   photoURL: "https://drive.google.com/uc?export=download&id=1_RWONcrIKve_93kXVYvASlwl1d88wQvw", // 照片，至少 300x300 像素以上
   facebookProfileLink: "https://fb.me/shengyoufan", // Facebook 個人社交連結
   twitterProfileLink: "https://twitter.com/shengyou", // Twitter 個人社交連結
-  otherLinks: null, // 其他連結 (ex: 個人網站 / 部落格)
+  otherLinks: "", // 其他連結 (ex: 個人網站 / 部落格)
   speechTopic: "一份 Zend Engine 外帶！透過 Micro 讓一次打包、多處運行變得可能", // 演講主題（35字內）
   speechSummary:
     "經過 27 年的發展，PHP 的許多部份已脫離了當初設計的使用方式，拿來寫 CLI 工具就是一個經典的例子。不過要執行 PHP 指令仍得依賴 Zend Engine，怎樣才能做到類似 Rust 或 Go 那種一次打包、多處執行的效果呢？Micro 專案給出了一個極具創意的答案：透過將 PHP 程式與輕量的 Zend Engine 打包，就能做出多個平台的 Binary 執行檔。在這場分享裡，將介紹 Micro 專案發起的緣由、底層的運作機制及可以應用的情境，並實際示範幾個應用程式打包的情境，打開大家對 PHP Runtime、應用程式打包及發佈的想像。", // 演講摘要
