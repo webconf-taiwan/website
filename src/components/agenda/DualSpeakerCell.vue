85f47c5c8f4652290bc6a6bad78d44a3a27541df
Alex Lau <alex.wanna.nirvana@gmail.com>
2023-07-13 18:26:27 +0800

    feat: 新增修改完的講者頭貼、修補其他 bug

--- src/components/agenda/DualSpeakerCell.vue
+++ src/components/agenda/DualSpeakerCell.vue
@@ -2,10 +2,10 @@
   <div class="h-full flex flex-col justify-between">
     <div class="mb-4 md:mb-0">
       <h2 class="text-white text-lg mb-2 md:leading-tight">
-        {{ speechTopic }}
+        {{ dualSpeakerInfo[0].speechTopic }}
       </h2>
       <div class="flex gap-2 mb-3 flex-wrap">
-        <CategoryTag :tag="tag" v-for="tag in categoryTags" :key="tag" />
+        <CategoryTag :tag="tag" v-for="tag in dualSpeakerInfo[0].categoryTags" :key="tag" />
       </div>
     </div>
 
@@ -13,13 +13,19 @@
       <div class="flex items-center gap-2">
         <img
           class="rounded-full h-10 w-10 border-custom-pink-700 border object-contain"
-          :src="`/speaker-img/${id}.jpg`"
+          :src="`/speaker-img/${dualSpeakerInfo[0].id}.jpg`"
         />
+        <img
+          class="rounded-full h-10 w-10 border-custom-pink-700 border object-contain -ml-5"
+          :src="`/speaker-img/${dualSpeakerInfo[1].id}.jpg`"
+        />
+
         <h3 class="text-custom-pink-700">
-          {{ speakerName }} <span v-if="altName !== ''">{{ `(${altName})` }}</span>
+          {{ dualSpeakerInfo[0].altName }} / <br />
+          {{ dualSpeakerInfo[1].altName }}
         </h3>
       </div>
-      <LocationBar :room="room" />
+      <LocationBar :room="dualSpeakerInfo[0].room" />
     </div>
   </div>
 </template>
@@ -29,23 +35,8 @@ import LocationBar from "@/components/agenda/LocationBar.vue";
 import CategoryTag from "../CategoryTag.vue";
 
 defineProps({
-  id: {
-    type: String,
-  },
-  speechTopic: {
-    type: String,
-  },
-  categoryTags: {
+  dualSpeakerInfo: {
     type: Array,
-  },
-  speakerName: {
-    type: String,
-  },
-  altName: {
-    type: String,
-  },
-  room: {
-    type: String,
   },
 });
 </script>
