6ae2b9fdbb85c3ca24ad78d9375ee08bc89af7bf
Alex Lau <alex.wanna.nirvana@gmail.com>
2023-07-13 18:26:27 +0800

    feat: 新增修改完的講者頭貼、修補其他 bug

--- src/components/agenda/GeneralRow.vue
+++ src/components/agenda/GeneralRow.vue
@@ -2,10 +2,10 @@
   <div class="flex-col md:flex-row flex min-h-[186px] gap-2">
     <TimelineCell :startTime="startTime" :endTime="endTime" />
     <div class="w-full md:basis-[90%] flex flex-col md:flex-row gap-2">
-      <template v-for="(speaker, index) in speakerInfoArr" :key="index">
+      <template v-for="(speaker, index) in formattedArr" :key="index">
         <div
           v-if="speaker !== null"
-          class="w-full md:basis-1/3 border border-custom-teal-500 p-3 md:px-5 md:py-4"
+          class="w-full md:basis-1/3 border border-custom-teal-500 p-3 md:px-5 md:py-4 speaker-cell"
           :class="{
             'md:block': speaker === null,
             'cursor-pointer': speaker !== null,
@@ -15,6 +15,7 @@
         >
           <SpeakerCell
             :speechTopic="speaker.speechTopic"
+            :id="speaker.id"
             :speakerName="speaker.speakerName"
             :altName="speaker.altName"
             :categoryTags="speaker.categoryTags"
@@ -22,13 +23,21 @@
           />
         </div>
         <div
-          v-else
-          class="w-full md:basis-1/3 border border-custom-teal-500 p-3 md:px-5 md:py-4"
+          v-if="speaker === null"
+          class="hidden md:basis-1/3 border border-custom-teal-500 p-3 md:px-5 md:py-4"
           :class="{
             'md:block': speaker === null,
           }"
         ></div>
       </template>
+
+      <div
+        v-if="isDualSpeaker"
+        class="cursor-pointer w-full md:basis-1/3 border border-custom-teal-500 p-3 md:px-5 md:py-4 speaker-cell"
+        @click="() => handleOpenSpeakerModal([DualArr[0].id, DualArr[1].id])"
+      >
+        <DualSpeakerCell :dualSpeakerInfo="DualArr" />
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +45,10 @@
 <script setup>
 import TimelineCell from "@/components/agenda/TimelineCell.vue";
 import SpeakerCell from "@/components/agenda/SpeakerCell.vue";
+// import { ref, watchEffect} from "vue";
+import { computed } from "vue";
+
+import DualSpeakerCell from "@/components/agenda/DualSpeakerCell.vue";
 
 const props = defineProps({
   speakerInfoArr: {
@@ -52,23 +65,76 @@ const props = defineProps({
   },
 });
 
-// function padEnd(array, minLength, fillValue = null) {
-//   return Object.assign(new Array(minLength).fill(fillValue), array);
-// }
+// const isDualSpeaker = ref(false);
+
+// const formattedArr = ref(JSON.parse(JSON.stringify(props.speakerInfoArr)));
+// const DualArr = ref([]);
+
+// watchEffect(() => {
+//   if (props.speakerInfoArr.some((speaker) => speaker?.id === "lin-yu-cheng")) {
+//     isDualSpeaker.value = true;
+//     formattedArr.value = [...props.speakerInfoArr];
+
+//     formattedArr.value = formattedArr.value.filter(
+//       (speaker) => speaker?.id !== "lin-yu-cheng" && speaker?.id !== "ke-ren-jie"
+//     );
+
+//     DualArr.value = props.speakerInfoArr.filter(
+//       (speaker) => speaker?.id === "lin-yu-cheng" || speaker?.id === "ke-ren-jie"
+//     );
+//   } else {
+//     isDualSpeaker.value = false;
+//     formattedArr.value = [...props.speakerInfoArr];
+//   }
+// });
+
+const isDualSpeaker = computed(() =>
+  props.speakerInfoArr.some((speaker) => speaker?.id === "lin-yu-cheng")
+);
+
+const formattedArr = computed(() => {
+  if (isDualSpeaker.value) {
+    return props.speakerInfoArr.filter(
+      (speaker) => speaker?.id !== "lin-yu-cheng" && speaker?.id !== "ke-ren-jie"
+    );
+  }
+
+  const nullCount = props.speakerInfoArr.filter((item) => item === null).length;
+
+  if (nullCount > 2) {
+    const removeDepNullArr = [...props.speakerInfoArr];
+    console.log(removeDepNullArr);
+    removeDepNullArr.splice(removeDepNullArr.lastIndexOf(null), 1);
+
+    return removeDepNullArr;
+  }
+
+  return props.speakerInfoArr;
+});
+
+const DualArr = computed(() => {
+  if (isDualSpeaker.value) {
+    return props.speakerInfoArr.filter(
+      (speaker) => speaker?.id === "lin-yu-cheng" || speaker?.id === "ke-ren-jie"
+    );
+  }
+
+  return [];
+});
 
 const handleOpenSpeakerModal = (speakerId) => {
   if (speakerId) {
     props.onOpenSpeakerModal(speakerId);
   }
 };
-
-// const handleOpenSpeakerModal = (speakerId) => () => {
-//   if (speakerId) {
-//     props.onOpenSpeakerModal(speakerId);
-//   }
-// };
-
-// const paddedSpeakerInfoArr = computed(() => padEnd(props.speakerInfoArr, 3, null));
 </script>
 
-<style scoped></style>
+<style scoped>
+.speaker-cell {
+  transition: background-color 0.3s;
+}
+
+.speaker-cell:hover {
+  background-color: #006a97;
+}
+</style>
