af67c895abf2b359d75282cf63625aad3f7322ef
Noi <aa8139106@gmail.com>
2023-07-15 22:12:09 +0800

    hide Information Incomplete Sponsor

--- src/content/sponsors.js
+++ src/content/sponsors.js
@@ -37,30 +37,30 @@ const sponsors = [
       },
     ],
   },
-  {
-    En_name: "platinum",
-    Zh_name: "白金級贊助商",
-    sponsors: [
-      {
-        id: "sponsor_picCollage",
-        sponsor_name: "PicCollage 拼貼趣",
-        image: { sponsorsPicCollage: true },
-        Introduction: [],
-      },
-    ],
-  },
-  {
-    En_name: "special",
-    Zh_name: "特別贊助",
-    sponsors: [
-      {
-        id: "sponsor_tenlong",
-        sponsor_name: "天瓏網路書店",
-        image: { sponsorsHexschool: true },
-        Introduction: [],
-      },
-    ],
-  },
+  // {
+  //   En_name: "platinum",
+  //   Zh_name: "白金級贊助商",
+  //   sponsors: [
+  //     {
+  //       id: "sponsor_picCollage",
+  //       sponsor_name: "PicCollage 拼貼趣",
+  //       image: { sponsorsPicCollage: true },
+  //       Introduction: [],
+  //     },
+  //   ],
+  // },
+  // {
+  //   En_name: "special",
+  //   Zh_name: "特別贊助",
+  //   sponsors: [
+  //     {
+  //       id: "sponsor_tenlong",
+  //       sponsor_name: "天瓏網路書店",
+  //       image: { sponsorsHexschool: true },
+  //       Introduction: [],
+  //     },
+  //   ],
+  // },
   {
     En_name: "silver",
     Zh_name: "銀牌贊助商",
@@ -95,12 +95,12 @@ const sponsors = [
     En_name: "organizer",
     Zh_name: "主辦廠商",
     sponsors: [
-      {
-        id: "sponsor_hexschool",
-        sponsor_name: "六角學院",
-        image: { sponsorsHexschool: true },
-        Introduction: [],
-      },
+      // {
+      //   id: "sponsor_hexschool",
+      //   sponsor_name: "六角學院",
+      //   image: { sponsorsHexschool: true },
+      //   Introduction: [],
+      // },
       {
         id: "sponsor_5xruby",
         sponsor_name: "五倍紅寶石",
