85a038c40609d44884cb661fa2216642f5cdcd7c
tingminitime <e0610k@gmail.com>
2024-09-28 16:03:02 +0800

    feat: replace space with venue, modify venue map

--- components/home/VenueInfoMarquee.vue
+++ components/home/VenueInfoMarquee.vue
@@ -11,14 +11,14 @@ const { reverse } = defineProps<{
       :loop="0"
       :auto-fill="true"
       :direction="reverse ? 'right' : 'left'"
-      class="overflow-y-clip"
+      class="pointer-events-none overflow-y-clip"
     >
       <span class="text-mina mr-4 bg-gradient-to-r from-[#00FFCC33] to-[#00FFCC33] bg-clip-text text-[32px] font-bold uppercase text-transparent lg:text-[80px]">
-        space information
+        venue information
       </span>
 
       <span class="text-mina marquee-text-outline mr-4 text-[32px] font-bold uppercase lg:text-[80px]">
-        space information
+        venue information
       </span>
     </NuxtMarquee>
   </div>
