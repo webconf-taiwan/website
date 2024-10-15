e5bdf325bd3619ab32bca4d604aca2a12e117855
roceil <96454995+roceil@users.noreply.github.com>
2024-10-15 22:10:40 +0800

    Feat/add footer bar (#91)
    * 修改 og image 路徑、修改講者照片、修改載入動畫時機 (#82)
    
    * fix(404): fix 404 layout
    
    * fix: modify og image path, modify speaker avatar, don't show loading animation except the home page
    
    ---------
    
    Co-authored-by: roceil <a0978006326@gmail.com>
    
    * feat: add new speaker, update README
    
    * chore: update deps
    
    * feat: add agenda page
    
    * fix(logo-animation): fixing logo animation when change route
    
    * Fix/nav links (#86)
    
    * 修改 og image 路徑、修改講者照片、修改載入動畫時機 ( by #82 ) (#83)
    
    * fix(404): fix 404 layout
    
    * fix: modify og image path, modify speaker avatar, don't show loading animation except the home page
    
    ---------
    
    Co-authored-by: roceil <a0978006326@gmail.com>
    
    * feat: add new speaker, update README (#85)
    
    * fix(nav): fix nav links on mobile
    
    ---------
    
    Co-authored-by: Tim Lin <44198700+tingminitime@users.noreply.github.com>
    
    * feat: add new speakers
    
    * feat: "即將開賣"改為"前往購票"
    
    * feat: add analytics script
    
    * 新增講者、修改購票連結按鈕顯示文字、載入外部 script ( same as #88 ) (#89)
    
    * 修改 og image 路徑、修改講者照片、修改載入動畫時機 ( by #82 ) (#83)
    
    * fix(404): fix 404 layout
    
    * fix: modify og image path, modify speaker avatar, don't show loading animation except the home page
    
    ---------
    
    Co-authored-by: roceil <a0978006326@gmail.com>
    
    * feat: add new speaker, update README (#85)
    
    * fix(nav): fix nav links on mobile (#87)
    
    * feat: add new speakers
    
    * feat: "即將開賣"改為"前往購票"
    
    * feat: add analytics script
    
    ---------
    
    Co-authored-by: roceil <a0978006326@gmail.com>
    Co-authored-by: roceil <96454995+roceil@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tim Lin <44198700+tingminitime@users.noreply.github.com>
    Co-authored-by: tingminitime <e0610k@gmail.com>

---
+++ components/countBar.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { useDateCountdown } from '@/composables/useCountDown'
+import { accupassLink } from '@/constants'
+
+
+const { remainingTime } = useDateCountdown()
+</script>
+
+<template>
+  <div class="fixed inset-x-0 bottom-0 z-50 w-full bg-black/80 px-5 py-3 md:px-0 md:py-3">
+    <div class="flex items-center justify-center space-x-3 md:space-x-8">
+      <div class="flex flex-col md:flex-row md:space-x-3">
+        <span class="ml-[-9px] font-bold text-primary-green">【即將開放報名】</span>
+        <span class="font-bold">{{ remainingTime.days }} 天 {{ remainingTime.hours }} 時 {{ remainingTime.minutes }} 分 {{ remainingTime.seconds }} 秒</span>
+      </div>
+      <Button
+        type="button"
+        variant="footerBar"
+        size="footerBar"
+      >
+        <NuxtLink
+          :to="accupassLink"
+          target="_blank"
+          class="text-base"
+        >
+          前往購票
+        </NuxtLink>
+      </Button>
+    </div>
+  </div>
+</template>
