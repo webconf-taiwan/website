83fe2bb30fb3a185d61c2c1b97a30faddb79218c
roceil <96454995+roceil@users.noreply.github.com>
2024-10-17 12:30:55 +0800

    feat(count-bar): update count bar message for 10/18 deadline (#100)

--- components/countBar.vue
+++ components/countBar.vue
@@ -9,7 +9,7 @@ const { remainingTime } = useDateCountdown()
   <div class="fixed inset-x-0 bottom-0 z-50 w-full bg-black/80 px-5 py-3 md:px-0 md:py-3">
     <div class="flex items-center justify-center space-x-3 md:space-x-8">
       <div class="flex flex-col md:flex-row md:space-x-3">
-        <span class="ml-[-9px] font-bold text-primary-green">【即將開放報名】</span>
+        <span class="ml-[-9px] font-bold text-primary-green">【超早鳥倒數一天】</span>
         <span class="font-bold">{{ remainingTime.days }} 天 {{ remainingTime.hours }} 時 {{ remainingTime.minutes }} 分 {{ remainingTime.seconds }} 秒</span>
       </div>
       <Button
