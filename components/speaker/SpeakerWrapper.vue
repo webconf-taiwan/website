fba8f2281d31ff7169af8eb76a9399a24f2ec534
roceil <96454995+roceil@users.noreply.github.com>
2024-11-12 14:57:51 +0800

    feat(speaker-wrapper): finish click speaker card to show agenda drawer (#106)

--- components/speaker/SpeakerWrapper.vue
+++ components/speaker/SpeakerWrapper.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 import { agendaData, agendaTags } from '~/constants/agendas'
 import { speakers } from '~/constants/speakers'
 
@@ -18,6 +19,23 @@ const speakersWithTags = computed(() => {
     }
   })
 })
+
+// 使用講者 code 取得議程資料
+function getAgendaDataBySpeakerCode(speakerCode: string) {
+  const speakerAgenda = Object.values(agendaData)
+    .flat()
+    .filter(slot => slot.type === 'agenda')
+    .flatMap((slot) => {
+      const agendas = slot.agendas || {}
+      if (slot.isBroadcast) {
+        return [agendas.M]
+      }
+      return Object.values(agendas)
+    })
+    .find(agenda => agenda?.speakerCodes.includes(speakerCode))
+
+  return speakerAgenda
+}
 
 // 根據選擇的標籤過濾講者
 const filteredSpeakers = computed(() => {
@@ -50,42 +68,84 @@ function getPersonalTags(speakerCode: string): string[] {
 
   return Array.from(tagsSet)
 }
+
+const agendasStore = useAgendasStore()
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const isSmallerLg = breakpoints.smaller('lg')
+const agendaDrawer = useTemplateRef('speakerDrawer')
+
+const drawerSlideDirection = computed(() => {
+  return isSmallerLg.value ? 'slide-up-full' : 'slide-left'
+})
+
+function triggerAgenda(speakerCode: string) {
+  const speakerAgenda = getAgendaDataBySpeakerCode(speakerCode)
+
+  if (speakerAgenda) {
+    agendasStore.currentAgendaDrawerId = speakerAgenda.id
+    agendasStore.setAgendaDrawerRenderData(speakerAgenda)
+    agendaDrawer.value?.open()
+  }
+}
 </script>
 
 <template>
-  <TransitionGroup
-    tag="ul"
-    name="speaker-list"
-    class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 lg:gap-y-16 xl:grid-cols-4"
-  >
-    <li
-      v-for="speaker in filteredSpeakers"
-      :key="speaker.id"
-      class="group relative flex w-full flex-col items-center px-4 py-[26px] duration-200 lg:hover:bg-primary-green/10"
+  <div>
+    <TransitionGroup
+      tag="ul"
+      name="speaker-list"
+      class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 lg:gap-y-16 xl:grid-cols-4"
     >
-      <SpeakerCard
-        :speaker="speaker"
-        :agenda-data="agendaData"
-      />
-    </li>
-  </TransitionGroup>
+      <li
+        v-for="speaker in filteredSpeakers"
+        :key="speaker.id"
+        class="group relative flex w-full flex-col items-center px-4 py-[26px] duration-200 lg:hover:bg-primary-green/10"
+      >
+        <button
+          type="button"
+          @click="triggerAgenda(speaker.code)"
+        >
+          <SpeakerCard
+            :speaker="speaker"
+            :agenda-data="agendaData"
+          />
+        </button>
+      </li>
+    </TransitionGroup>
+
+    <Teleport to="body">
+      <Drawer
+        ref="speakerDrawer"
+        :slide-direction="drawerSlideDirection"
+        drawer-class="lg:w-[60dvw]"
+        @close="agendasStore.resetContentTab"
+      >
+        <DrawerContentLayout>
+          <template #content>
+            <AgendaDrawerWrapper />
+          </template>
+        </DrawerContentLayout>
+      </Drawer>
+    </Teleport>
+  </div>
 </template>
 
 <style scoped>
-.speaker-list-move,
-.speaker-list-enter-active,
-.speaker-list-leave-active {
-  transition: all 0.5s ease;
-}
+@media (min-width: 1024px) {
+  .speaker-list-move,
+  .speaker-list-enter-active,
+  .speaker-list-leave-active {
+    transition: all 0.5s ease;
+  }
 
-.speaker-list-enter-from,
-.speaker-list-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
-}
+  .speaker-list-enter-from,
+  .speaker-list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
 
-/* 確保離開的項目不會影響佈局 */
-.speaker-list-leave-active {
-  position: absolute;
+  .speaker-list-leave-active {
+    position: absolute;
+  }
 }
 </style>
