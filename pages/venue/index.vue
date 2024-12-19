abeaeea7bb2cb10c7f855e38ef27a67d39ae3805
roceil <a0978006326@gmail.com>
2024-12-19 17:47:18 +0800

    feat(venue-info): deving venue info

--- pages/venue/index.vue
+++ pages/venue/index.vue
@@ -42,7 +42,7 @@ onMounted(() => {
     </ClientOnly>
 
     <div class="mx-auto">
-      <SectionTitle class="mb-5 justify-center lg:mb-12">
+      <SectionTitle class="mb-5 justify-start lg:mb-12">
         <template #title>
           VENUE
         </template>
@@ -50,6 +50,36 @@ onMounted(() => {
           場域
         </template>
       </SectionTitle>
+
+      <section class="flex flex-col justify-center">
+        <NuxtImg
+          src="/venue/location.png"
+          alt="場域外觀"
+          class="w-full"
+        />
+
+        <div class="relative mt-5 p-5">
+          <!-- 四邊定位的正方形 -->
+          <div class="absolute inset-0 size-[10px] border-l border-t border-primary-green"></div>
+          <div class="absolute bottom-0 right-0 size-[10px] border-b border-r border-primary-green"></div>
+          <div class="absolute bottom-0 left-0 size-[10px] border-b border-l border-primary-green"></div>
+          <div class="absolute right-0 top-0 size-[10px] border-r border-t border-primary-green"></div>
+
+          <div class="flex items-center space-x-3">
+            <h2 class="text-xl font-bold">
+              瓶蓋工廠台北製造所
+            </h2>
+            <Icon
+              name="heroicons:arrow-top-right-on-square"
+              class="text-2xl text-primary-green"
+            />
+          </div>
+
+          <p class="mt-4">
+            115 台北市南港區南港路二段 13 號
+          </p>
+        </div>
+      </section>
     </div>
   </main>
 </template>
