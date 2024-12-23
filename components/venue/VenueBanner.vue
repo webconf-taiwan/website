fc97b4ffc389629c4a02f1161f6f6a01e1f3136d
roceil <a0978006326@gmail.com>
2024-12-23 15:02:12 +0800

    chore(venue/banner): update venue banner img

--- components/venue/VenueBanner.vue
+++ components/venue/VenueBanner.vue
@@ -3,25 +3,27 @@ import { venueGoogleMapLink } from '~/constants'
 </script>
 
 <template>
-  <section class="mb-10 flex flex-col justify-center lg:mb-[100px] lg:flex-row lg:justify-between">
+  <section class="mx-auto mb-10 flex max-w-[640px] flex-col justify-center lg:mx-0 lg:mb-[100px] lg:max-w-none lg:flex-row lg:justify-between">
     <NuxtImg
-      src="/venue/location.svg"
+      src="/venue/location.png"
       alt="場域外觀"
       class="w-full lg:hidden"
       :placeholder="[32, 32, 80, 5]"
+      format="webp"
       draggable="false"
     />
 
     <NuxtImg
-      src="/venue/location-lg.svg"
+      src="/venue/location-lg.png"
       alt="場域外觀"
       class="hidden w-[66%] lg:block "
       :placeholder="[32, 32, 80, 5]"
+      format="webp"
       draggable="false"
     />
 
     <NuxtLink
-      class="group relative mt-5 p-5 lg:mt-auto xl:px-7"
+      class="group relative mt-5 flex flex-col p-5 lg:mt-auto xl:px-7 items-center"
       :to="venueGoogleMapLink"
       target="_blank"
       external
@@ -32,21 +34,23 @@ import { venueGoogleMapLink } from '~/constants'
       <div class="absolute bottom-0 left-0 size-[10px] border-b border-l border-primary-green duration-300 ease-in-out lg:group-hover:bottom-1 lg:group-hover:left-1"></div>
       <div class="absolute right-0 top-0 size-[10px] border-r border-t border-primary-green duration-300 ease-in-out lg:group-hover:right-1 lg:group-hover:top-1"></div>
 
-      <div class="group flex items-center space-x-3">
-        <div
-          class="text-h3 transition lg:text-md-h3 lg:underline-offset-4 lg:group-hover:text-primary-green lg:group-hover:underline"
-        >
-          瓶蓋工廠台北製造所
-          <Icon
-            name="heroicons:arrow-top-right-on-square"
-            class="size-6 text-primary-green"
-          />
+      <div class="inset-0">
+        <div class="group flex items-center space-x-3">
+          <div
+            class="text-h3 transition lg:text-md-h3 lg:underline-offset-4 lg:group-hover:text-primary-green lg:group-hover:underline"
+          >
+            瓶蓋工廠台北製造所
+            <Icon
+              name="heroicons:arrow-top-right-on-square"
+              class="size-6 text-primary-green"
+            />
+          </div>
         </div>
-      </div>
 
-      <p class="mt-4">
-        115 台北市南港區南港路二段 13 號
-      </p>
+        <p class="mt-4">
+          115 台北市南港區南港路二段 13 號
+        </p>
+      </div>
     </NuxtLink>
   </section>
 </template>
