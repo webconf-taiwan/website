8c21b5cf53cfbc85708de6dd526236463334af1c
roceil <a0978006326@gmail.com>
2024-12-27 22:54:49 +0800

    fix(drawer): 註解獨立頁面 onUnmounted

--- pages/agenda/[id].vue
+++ pages/agenda/[id].vue
@@ -88,8 +88,9 @@ const ogImageOptions = {
 defineOgImage(ogImageOptions)
 
 onUnmounted(() => {
-  console.log("onUnmounted 觸發：把 singleAgendaMarkdownData 設為 null")
-  agendasStore.singleAgendaMarkdownData = null
+  console.log("onUnmounted 觸發")
+  // console.log("onUnmounted 觸發：把 singleAgendaMarkdownData 設為 null")
+  // agendasStore.singleAgendaMarkdownData = null
 })
 </script>
 
