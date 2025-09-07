9cd3e3f314dec1bdd40ff5e5df3a502c6fcfa698
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2025-09-07 23:48:58 +0800

    feat: 整併 Hero、About、CTA、COC、Footer 等區塊

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,16 @@
 <template>
   <ShareBgBorder>
     <HomeNavbar />
-    <div class="relative z-10 w-full">
-      <img
-        class="mt-[-4%] w-full"
-        src="/images/heroBg.png"
-        alt="hero 背景圖片"
-      />
-      <div class="relative"></div>
-    </div>
+    <HomeHero />
     <div
       id="about-section"
       class="relative z-0 min-h-screen w-full"
     >
       <HomeAbout />
     </div>
-    <div class="relative z-0 min-h-screen w-full"></div>
+    <HomeOrganizersMarquee />
+    <HomeCTA />
+    <HomeCocNotice />
     <HomeSiteFooter />
-    <HomeCopyRight />
   </ShareBgBorder>
 </template>
-
-<style>
-.bg {
-  width: 100%;
-  height: calc(100vh - 2.5rem);
-  background-image: url("/images/heroBg.png");
-  background-repeat: no-repeat;
-  background-position: right bottom;
-  background-size: 100% auto;
-}
-</style>
