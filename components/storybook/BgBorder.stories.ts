d04a99d5f71d18255f3084eb063db200d49f5587
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2025-09-08 10:37:43 +0800

    fix: 修復 BgBorder DVD Dot 成畫面高度(不隨卷軸改變) & 新增 Storybook 示範檔

---
+++ components/storybook/BgBorder.stories.ts
@@ -0,0 +1,32 @@
+import BgBorder from '../share/BgBorder.vue'
+
+export default {
+  title: 'Components/BgBorder',
+  component: BgBorder,
+  argTypes: {
+    dvdDotSpeed: {
+      control: { type: 'range', min: 1, max: 10, step: 1 },
+    },
+    dvdDotColors: {
+      control: { type: 'array' },
+    },
+  },
+}
+
+export const Playground = {
+  args: {
+    dvdDotSpeed: 3,
+    dvdDotColors: ['#2F2ADB', '#919191', '#E6E6E6'],
+    default: '網格 + DVD Dot 效果背景',
+  },
+  render: (args: any) => ({
+    components: { BgBorder },
+    setup() {
+      return { args }
+    },
+    template: `<BgBorder v-bind="args">
+        <h2 class="h-screen grid place-content-center text-white text-2xl">{{ args.default }}</h2>
+    </BgBorder>
+    `,
+  }),
+}
