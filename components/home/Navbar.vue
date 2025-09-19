3ed91432f000033729cb12862b596034926f5a56
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2025-09-20 01:02:40 +0800

    feat: rwd 調整 Navbar 排版方式

--- components/home/Navbar.vue
+++ components/home/Navbar.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
 const NAV_ITEMS = [
-  { name: '議程資訊', href: '#' },
-  { name: '講者介紹', href: '#' },
-  { name: '場域介紹', href: '#' },
-  { name: '贊助廠商', href: '#' },
-  { name: '主辦團隊', href: '#' },
-  { name: '歷屆回顧', href: '#' },
+  { name: '議程資訊', href: '#', isTablet: true, isDesktop: true },
+  { name: '講者介紹', href: '#', isTablet: false, isDesktop: true },
+  { name: '場域介紹', href: '#', isTablet: true, isDesktop: true },
+  { name: '贊助廠商', href: '#', isTablet: false, isDesktop: true },
+  { name: '主辦團隊', href: '#', isTablet: false, isDesktop: true },
+  { name: '歷屆回顧', href: '#', isTablet: false, isDesktop: true },
 ]
+const isSaleOpen = useGlobalState().isSaleOpen
 </script>
 
 <template>
   <header
     class="sticky top-0 z-50 w-full border-b border-b-webconf-gray bg-black"
   >
-    <div class="container flex items-center justify-between sm:px-12 lg:px-20">
+    <div
+      class="container flex items-center justify-between px-5 sm:px-8 lg:px-20"
+    >
+      <!-- logo -->
       <h1 v-cursor="{ scale: 0.4, duration: 0.5 }">
         <span class="sr-only">2025 WebConf</span>
         <NuxtLink
@@ -41,24 +45,39 @@ const NAV_ITEMS = [
         </NuxtLink>
       </h1>
 
-      <nav>
-        <ul
-          class="flex py-3 font-semibold leading-[1.6] tracking-[.04em] text-webconf-gray"
-        >
-          <li
-            v-for="navItem in NAV_ITEMS"
-            :key="navItem.name"
-          >
-            <NuxtLink
-              v-cursor="{ scale: 0.4, duration: 0.5 }"
-              class="inline-block px-3 py-1 transition-colors duration-500 hover:text-webconf-blue"
-              :to="navItem.href"
+      <!-- 導覽列 -->
+      <div class="flex items-center gap-4 py-3 text-btn-16 text-webconf-gray">
+        <nav>
+          <ul class="flex py-[3px]">
+            <li
+              v-for="navItem in NAV_ITEMS"
+              :key="navItem.name"
+              class="hidden"
+              :class="{
+                'sm:block': navItem.isTablet,
+                'lg:block': navItem.isDesktop,
+              }"
             >
-              {{ navItem.name }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
+              <NuxtLink
+                v-cursor="{ scale: 0.4, duration: 0.5 }"
+                class="inline-block px-3 py-1 transition-colors duration-500 hover:text-webconf-blue"
+                :to="navItem.href"
+              >
+                {{ navItem.name }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </nav>
+
+        <NuxtLink
+          to="#"
+          :class="{ hidden: isSaleOpen }"
+        >
+          <ShareTag size="lg">
+            前往購票
+          </ShareTag>
+        </NuxtLink>
+      </div>
     </div>
   </header>
 </template>
