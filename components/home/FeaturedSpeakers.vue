1902650a83dc8c2412ee91fe6ad619df236b4e58
Shin <penspulse@gmail.com>
2025-09-25 15:34:11 +0800

    feat: 新增輪播控制按鈕組

--- components/home/FeaturedSpeakers.vue
+++ components/home/FeaturedSpeakers.vue
@@ -127,5 +127,23 @@ function handlePrev() {
         ></button>
       </div>
     </section>
+
+    <!-- 輪播控制 -->
+    <div
+      class="grid grid-cols-1 gap-6 pb-[132px] pt-6 md:grid-cols-3 md:px-8 md:pb-20 md:pt-10 xl:hidden"
+    >
+      <ShareSlideController
+        @next="handleNext"
+        @prev="handlePrev"
+      />
+      <div class="justify-self-center">
+        <ShareLinkButton
+          to="#"
+          class="font-semibold"
+        >
+          更多講者
+        </ShareLinkButton>
+      </div>
+    </div>
   </div>
 </template>
