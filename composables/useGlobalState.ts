ef2e60653a667621ef9575ef51b275f8f188d65d
Shin <akiyachiumi@gmail.com>
2025-10-02 20:46:40 +0800

    feat: 新增 /agenda 頁面

--- composables/useGlobalState.ts
+++ composables/useGlobalState.ts
@@ -2,7 +2,7 @@ export function useGlobalState() {
   // 是否已經開賣
   const isSaleOpen = useState<boolean>('isSaleOpen', () => false)
   // 是否已經完成初次加載動畫
-  const isFirstLoad = useState<boolean>('isFirstLoad', () => false)
+  const isFirstLoad = useState<boolean>('isFirstLoad', () => true)
 
   return {
     isSaleOpen: readonly(isSaleOpen),
---
+++ pages/agenda.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- banner -->
+  <h1 class="bg-white text-[100px] text-black">
+    議程頁
+  </h1>
+
+  <!-- 活動日期 + 箭頭動畫 -->
+
+  <!-- 列表 -->
+
+  <!-- 提示 -->
+</template>
