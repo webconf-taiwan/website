6c09883e96ecc663d36cab5c94dc65e349f48ebf
Shin <akiyachiumi@gmail.com>
2025-10-02 20:57:03 +0800

    feat: 新增活動日期並套上箭頭動畫

--- pages/agenda.vue
+++ pages/agenda.vue
@@ -1,10 +1,16 @@
 <template>
   <!-- banner -->
-  <h1 class="bg-white text-[100px] text-black">
+  <h1 class="bg-red-500 text-[100px] text-black">
     議程頁
   </h1>
 
   <!-- 活動日期 + 箭頭動畫 -->
+  <div
+    v-arrow="{ speed1: '12s', color: '#E6E6E6' }"
+    class="border-y border-webconf-gray px-5 py-4 text-h4-24 text-white md:px-20 md:py-6"
+  >
+    Dec. 12-13
+  </div>
 
   <!-- 列表 -->
 
