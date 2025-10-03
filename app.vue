9740f8281dde9f0a254877645e4b8ebcfd487deb
Shin <penspulse@gmail.com>
2025-10-03 20:58:11 +0800

    chore: 於 head 新增外部觀測 (#176)

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 const { isFirstLoad } = useGlobalState()
 
+useHead({
+  script: [
+    {
+      'defer': true,
+      'src': 'https://a.5xcamp.us/script.js',
+      'data-website-id': '41664933-5ac4-4e59-ad44-2b69cc1dbbf4',
+    },
+  ],
+})
+
 defineOgImage({
   url: 'https://webconf.tw/images/seo/ogImage.png',
   width: 1200,
