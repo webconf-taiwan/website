2fb620cac11fdf52db8d1cc60e0b30f3fb0b844c
Shin <akiyachiumi@gmail.com>
2025-10-21 04:34:20 +0800

    feat: 分離日期切換按鈕並新增廳號標示

---
+++ components/agenda/DateToggler.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const selectedDate = defineModel<string>({ required: true })
+</script>
+
+<template>
+  <div class="flex items-center justify-end gap-2 lg:ml-4 2xl:w-[260px]">
+    <span>DEC. </span>
+
+    <div
+      v-cursor="{
+        scale: 0.5,
+        duration: 0.5,
+        backgroundColor: 'rgba(0, 46, 255, 0.9)',
+      }"
+      class="flex h-10 w-[213px] border border-webconf-blue lg:h-[46px] lg:w-[150px]"
+    >
+      <button
+        type="button"
+        class="flex-center w-full duration-300"
+        :class="
+          selectedDate === '12'
+            ? 'bg-webconf-blue text-white'
+            : 'text-webconf-gray'
+        "
+        aria-label="篩選 12 月 12 日的議程"
+        @click="selectedDate = '12'"
+      >
+        12
+      </button>
+      <button
+        type="button"
+        class="flex-center w-full duration-300"
+        :class="
+          selectedDate === '13'
+            ? 'bg-webconf-blue text-white'
+            : 'text-webconf-gray'
+        "
+        aria-label="篩選 12 月 13 日的議程"
+        @click="selectedDate = '13'"
+      >
+        13
+      </button>
+    </div>
+  </div>
+</template>
