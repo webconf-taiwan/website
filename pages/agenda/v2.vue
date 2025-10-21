d6af1686daa37a13e459fb05fa00836c89ea30ea
Shin <penspulse@gmail.com>
2025-10-21 12:23:45 +0800

    feat: 新增桌面版議程列表 grid 排版

--- pages/agenda/v2.vue
+++ pages/agenda/v2.vue
@@ -3,260 +3,224 @@ useSeoMeta({
   title: '議程資訊',
 })
 
-// type AgendaTag
-//   = | 'AI'
-//     | 'Frontend'
-//     | 'Backend'
-//     | 'Security'
-//     | '軟體設計'
-//     | '設計實務'
-//     | '產品思維'
-//     | '產業應用'
-//     | '團隊管理'
-//     | 'Agile'
+type AgendaTag
+  = | 'AI'
+    | 'Frontend'
+    | 'Backend'
+    | 'Security'
+    | '軟體設計'
+    | '設計實務'
+    | '產品思維'
+    | '產業應用'
+    | '團隊管理'
+    | 'Agile'
 
-// interface SpeakerInfo {
-//   name: string
-//   title: string
-// }
+interface SpeakerInfo {
+  name: string
+  avatarUrl: string
+}
 
-// interface AgendaItem {
-//   topic: string
-//   speakerInfo: SpeakerInfo[]
-//   tags: AgendaTag[]
-// }
+interface AgendaItem {
+  topic: string
+  speakerInfo: SpeakerInfo[]
+  tags: AgendaTag[]
+  day: '12' | '13'
+  startTime: string
+}
 
-// const AGENDA_LIST: AgendaItem[] = [
-//   {
-//     topic: 'B2B 服務的 AI Agent 產品設計原則',
-//     speakerInfo: [
-//       {
-//         name: 'Happy',
-//         title: '91APP / 產品長',
-//       },
-//     ],
-//     tags: ['AI', '產品思維', '產業應用', '團隊管理'],
-//   },
-//   {
-//     topic: '從冷知識到漏洞：你不懂的 Web，駭客懂',
-//     speakerInfo: [
-//       {
-//         name: '胡立',
-//         title: `技術部落格 Huli's blog / 站長`,
-//       },
-//     ],
-//     tags: ['Frontend', 'Backend', 'Security'],
-//   },
-//   {
-//     topic: '程式碼與尿布：媽媽工程師的生存指南',
-//     speakerInfo: [
-//       {
-//         name: 'Hannah',
-//         title: 'Remote / Frontend ',
-//       },
-//     ],
-//     tags: ['Frontend'],
-//   },
-//   {
-//     topic: '大 AI 時代，工程師的成長之路 / 從 Junior 到 Staff',
-//     speakerInfo: [
-//       {
-//         name: '奶綠茶',
-//         title: 'PositiveGrid Staff Frontend Engineer',
-//       },
-//     ],
-//     tags: ['Frontend'],
-//   },
-//   {
-//     topic: '別再瞎忙了！讓 AI 幫產品團隊找到對的問題',
-//     speakerInfo: [
-//       {
-//         name: 'Peter',
-//         title: '漸強實驗室 / Product Lead',
-//       },
-//     ],
-//     tags: ['AI', '產品思維', '團隊管理', 'Agile'],
-//   },
-//   {
-//     topic: 'AI 時代下， Product Sense 就是你的秘密武器',
-//     speakerInfo: [
-//       {
-//         name: 'Chris',
-//         title: 'PicCollage 拼貼趣 <br /> / 資深產品設計經理',
-//       },
-//       {
-//         name: 'Tiyna',
-//         title: 'PicCollage 拼貼趣 <br /> / 資深產品設計師',
-//       },
-//     ],
-//     tags: ['AI', '設計實務', '產品思維'],
-//   },
-//   {
-//     topic: '軟體開發邪教的救贖：AI 時代更應掌握的 TDD 技能',
-//     speakerInfo: [
-//       {
-//         name: 'Kuma Syu',
-//         title: '緯雲有限公司 / R&D Manager',
-//       },
-//     ],
-//     tags: ['AI', '軟體設計', '產品思維'],
-//   },
-//   {
-//     topic: '零基礎打造 400 萬用戶，我們犯了哪些錯誤',
-//     speakerInfo: [
-//       {
-//         name: 'Max Chen',
-//         title: '對稱資訊股份有限公司 / 面試趣 CEO',
-//       },
-//     ],
-//     tags: ['產品思維', '產業應用'],
-//   },
-//   {
-//     topic: '產品 OKR 的訂立與 Roadmap 展開',
-//     speakerInfo: [
-//       {
-//         name: '曾友志',
-//         title: '下午先生有限公司 / 資深產品顧問',
-//       },
-//     ],
-//     tags: ['產品思維', '團隊管理'],
-//   },
-//   {
-//     topic: 'React 優化實戰分析 / 掌握 React 進階技術 x 底層思維',
-//     speakerInfo: [
-//       {
-//         name: 'ThisWeb (Kun)',
-//         title: 'ThisWeb / 前端工程師',
-//       },
-//     ],
-//     tags: ['Frontend'],
-//   },
-//   {
-//     topic: '願 Web API 原力與你同在',
-//     speakerInfo: [
-//       {
-//         name: 'MUKI',
-//         title: 'MUKI space* / 前端工程師',
-//       },
-//     ],
-//     tags: ['AI', 'Frontend'],
-//   },
-//   {
-//     topic: '掌握田野中的「人」：真實場域研究的人際溝通與信任建立',
-//     speakerInfo: [
-//       {
-//         name: 'Joey',
-//         title: '引鹿創新體驗研究室  / UX 總監',
-//       },
-//     ],
-//     tags: ['設計實務', '團隊管理'],
-//   },
-//   {
-//     topic: '以打詐為例，服務設計如何讓公共數位服務有感',
-//     speakerInfo: [
-//       {
-//         name: '卓致遠',
-//         title: '致遠體驗設計 / 體驗總監',
-//       },
-//     ],
-//     tags: ['設計實務', '產品思維'],
-//   },
-//   {
-//     topic: '服務設計師的傳說：90% 的時間在讓設計發生',
-//     speakerInfo: [
-//       {
-//         name: '郭心喻',
-//         title:
-//           'Government of Nova Scotia 加拿大新斯科舍省政府 <br /> / Senior Service Designer 資深服務設計師',
-//       },
-//     ],
-//     tags: ['軟體設計', '設計實務', '產品思維', '產業應用'],
-//   },
-//   {
-//     topic: '從設計到共識：悠識如何在每一次新專案裡，用溝通建立信任關係',
-//     speakerInfo: [
-//       {
-//         name: '林星妤',
-//         title: '悠識數位 / Senior Experience Designer',
-//       },
-//       {
-//         name: '黃明硯',
-//         title: '悠識數位 /  Experience Designer',
-//       },
-//     ],
-//     tags: ['設計實務', '產品思維'],
-//   },
-//   {
-//     topic: '從研究到上線：全流程設計與 AI 協作專案實戰',
-//     speakerInfo: [
-//       {
-//         name: 'Niki Liu',
-//         title: '新加坡商鈦坦科技 / Senior Product Designer',
-//       },
-//     ],
-//     tags: ['AI', '設計實務', '產品思維'],
-//   },
-//   {
-//     topic: '從使用者到工程師：AI 在企業怎麼落地？',
-//     speakerInfo: [
-//       {
-//         name: 'Shirney Huang 黃琇琳',
-//         title: 'Aiworks / CEO',
-//       },
-//     ],
-//     tags: ['AI'],
-//   },
-//   {
-//     topic: 'GenAI 時代下的測試三板斧',
-//     speakerInfo: [
-//       {
-//         name: '柯仁傑',
-//         title: 'Odd-e / Technical Coach',
-//       },
-//     ],
-//     tags: ['軟體設計'],
-//   },
-//   {
-//     topic: '培養十倍速 PM：從工具到習慣',
-//     speakerInfo: [
-//       {
-//         name: '趙柏強',
-//         title: 'Stable Progress / Growth Companion',
-//       },
-//     ],
-//     tags: ['AI', '產品思維', '團隊管理', 'Agile'],
-//   },
-//   {
-//     topic: '對微前端的美好想像',
-//     speakerInfo: [
-//       {
-//         name: 'Eric Lee',
-//         title: '前端輕鬆聊 / Senior Software Engineer',
-//       },
-//     ],
-//     tags: ['Frontend', '軟體設計'],
-//   },
-//   {
-//     topic: 'AI 時代下， 如何運用數據幫助產品發揮影響力',
-//     speakerInfo: [
-//       {
-//         name: '姜乃文',
-//         title: '天下雜誌集團 / 主任分析師',
-//       },
-//     ],
-//     tags: ['AI', '產品思維', '產業應用', '團隊管理'],
-//   },
-//   {
-//     topic: '走在同業前面：國泰的雲端轉型洞察與啟示',
-//     speakerInfo: [
-//       {
-//         name: '顏勝豪 Otto',
-//         title: '國泰金控  / 協理',
-//       },
-//     ],
-//     tags: ['AI', '產業應用'],
-//   },
-// ]
+interface AgendaCoStream extends Pick<AgendaItem, 'day' | 'startTime'> {
+  label: '同步聯播'
+}
+
+const AGENDA_LIST: (AgendaItem | AgendaCoStream)[] = [
+  // 12/12 09:00
+  {
+    topic: 'B2B 服務的 AI Agent 產品設計原則',
+    speakerInfo: [
+      {
+        name: 'Happy',
+        avatarUrl: '91APP / 產品長',
+      },
+    ],
+    tags: ['AI', '產品思維', '產業應用', '團隊管理'],
+    day: '12',
+    startTime: '09:00',
+  },
+  {
+    topic: '從冷知識到漏洞：你不懂的 Web，駭客懂',
+    speakerInfo: [
+      {
+        name: '胡立',
+        avatarUrl: `技術部落格 Huli's blog / 站長`,
+      },
+    ],
+    tags: ['Frontend', 'Backend', 'Security'],
+    day: '12',
+    startTime: '10:00',
+  },
+  {
+    topic: '程式碼與尿布：媽媽工程師的生存指南',
+    speakerInfo: [
+      {
+        name: 'Hannah',
+        avatarUrl: 'Remote / Frontend ',
+      },
+    ],
+    tags: ['Frontend'],
+    day: '12',
+    startTime: '11:00',
+  },
+  // 12/12 10:05
+  {
+    topic: '大 AI 時代，工程師的成長之路 / 從 Junior 到 Staff',
+    speakerInfo: [
+      {
+        name: '奶綠茶',
+        avatarUrl: 'PositiveGrid Staff Frontend Engineer',
+      },
+    ],
+    tags: ['Frontend'],
+    day: '12',
+    startTime: '10:05',
+  },
+  {
+    topic: '別再瞎忙了！讓 AI 幫產品團隊找到對的問題',
+    speakerInfo: [
+      {
+        name: 'Peter',
+        avatarUrl: '漸強實驗室 / Product Lead',
+      },
+    ],
+    tags: ['AI', '產品思維', '團隊管理', 'Agile'],
+    day: '12',
+    startTime: '10:10',
+  },
+  {
+    topic: 'AI 時代下， Product Sense 就是你的秘密武器',
+    speakerInfo: [
+      {
+        name: 'Chris',
+        avatarUrl: 'PicCollage 拼貼趣 <br /> / 資深產品設計經理',
+      },
+      {
+        name: 'Tiyna',
+        avatarUrl: 'PicCollage 拼貼趣 <br /> / 資深產品設計師',
+      },
+    ],
+    tags: ['AI', '設計實務', '產品思維'],
+    day: '12',
+    startTime: '10:15',
+  },
+  // 12/12 11:00
+  {
+    topic: '軟體開發邪教的救贖：AI 時代更應掌握的 TDD 技能',
+    speakerInfo: [
+      {
+        name: 'Kuma Syu',
+        avatarUrl: '緯雲有限公司 / R&D Manager',
+      },
+    ],
+    tags: ['AI', '軟體設計', '產品思維'],
+    day: '12',
+    startTime: '11:00',
+  },
+  {
+    topic: '零基礎打造 400 萬用戶，我們犯了哪些錯誤',
+    speakerInfo: [
+      {
+        name: 'Max Chen',
+        avatarUrl: '對稱資訊股份有限公司 / 面試趣 CEO',
+      },
+    ],
+    tags: ['產品思維', '產業應用'],
+    day: '12',
+    startTime: '11:05',
+  },
+  {
+    topic: '產品 OKR 的訂立與 Roadmap 展開',
+    speakerInfo: [
+      {
+        name: '曾友志',
+        avatarUrl: '下午先生有限公司 / 資深產品顧問',
+      },
+    ],
+    tags: ['產品思維', '團隊管理'],
+    day: '12',
+    startTime: '11:10',
+  },
+  // 12/12 13:30
+  {
+    topic: 'React 優化實戰分析 / 掌握 React 進階技術 x 底層思維',
+    speakerInfo: [
+      {
+        name: 'ThisWeb (Kun)',
+        avatarUrl: 'ThisWeb / 前端工程師',
+      },
+    ],
+    tags: ['Frontend'],
+    day: '12',
+    startTime: '13:30',
+  },
+  {
+    topic: '願 Web API 原力與你同在',
+    speakerInfo: [
+      {
+        name: 'MUKI',
+        avatarUrl: 'MUKI space* / 前端工程師',
+      },
+    ],
+    tags: ['AI', 'Frontend'],
+    day: '12',
+    startTime: '13:30',
+  },
+  {
+    topic: '掌握田野中的「人」：真實場域研究的人際溝通與信任建立',
+    speakerInfo: [
+      {
+        name: 'Joey',
+        avatarUrl: '引鹿創新體驗研究室  / UX 總監',
+      },
+    ],
+    tags: ['設計實務', '團隊管理'],
+    day: '12',
+    startTime: '13:30',
+  },
+  // 12/12 14:25
+  {
+    topic: '以打詐為例，服務設計如何讓公共數位服務有感',
+    speakerInfo: [
+      {
+        name: '卓致遠',
+        avatarUrl: '致遠體驗設計 / 體驗總監',
+      },
+    ],
+    tags: ['設計實務', '產品思維'],
+    day: '12',
+    startTime: '14:25',
+  },
+  {
+    label: '同步聯播',
+    day: '12',
+    startTime: '14:25',
+  },
+  {
+    label: '同步聯播',
+    day: '12',
+    startTime: '14:25',
+  },
+]
+
+const agendasAtDec12Morning = computed(() => {
+  return AGENDA_LIST.filter(
+    item => item.day === '12' && item.startTime < '12:00',
+  )
+})
+
+const agendasAtDec12Afternoon = computed(() => {
+  return AGENDA_LIST.filter(
+    item => item.day === '12' && item.startTime >= '12:00',
+  )
+})
 
 const selectedDate = ref(new Date() <= new Date('2025-12-13') ? '12' : '13')
 </script>
@@ -347,7 +311,7 @@ const selectedDate = ref(new Date() <= new Date('2025-12-13') ? '12' : '13')
     >
       <div class="flex-center py-[6px] text-h4-24 lg:py-3">
         <!-- 日期篩選 -->
-        <AgendaDateToggler v-model="selectedDate" />
+        <AgendaDaySwitch v-model="selectedDate" />
 
         <!-- 議程類型篩選 -->
         <AgendaTopicFilter
@@ -379,9 +343,9 @@ const selectedDate = ref(new Date() <= new Date('2025-12-13') ? '12' : '13')
     <section class="flex">
       <!-- 議程類型篩選清單 -->
       <aside
-        class="hidden flex-col items-start gap-2 border-r border-webconf-gray bg-black p-4 lg:flex lg:w-[228px] lg:pl-4 2xl:w-[260px] 2xl:pl-12"
+        class="hidden shrink-0 flex-col items-start gap-2 border-r border-webconf-gray bg-black p-4 lg:flex lg:w-[228px] lg:pl-4 2xl:w-[260px] 2xl:pl-12"
       >
-        <AgendaTopicFilter size="lg" />
+        <AgendaTagFilter size="lg" />
         <span
           class="topic-filter-title inline-block text-left text-h4-60 text-webconf-blue"
         >
@@ -389,16 +353,37 @@ const selectedDate = ref(new Date() <= new Date('2025-12-13') ? '12' : '13')
         </span>
       </aside>
 
-      <div class="flex grow bg-black">
-        <div class="flex-1 lg:w-[228px]">
-          議程1
+      <div
+        v-cursor="{
+          scale: 0.5,
+          duration: 0.5,
+        }"
+        class="grid grid-cols-3 gap-[0.5px] bg-webconf-gray/50"
+      >
+        <!-- 上午議程 -->
+        <NuxtLink
+          v-for="(agenda, index) in agendasAtDec12Morning"
+          :key="`${agenda.startTime}-${index}`"
+          class="col-span-1 bg-black hover:bg-webconf-blue lg:h-[228px]"
+          to="/agenda/v2"
+        >
+          {{ agenda.topic || agenda.label }}
+        </NuxtLink>
+
+        <!-- 中午休息 -->
+        <div class="col-span-3 px-14 text-center text-h4-24">
+          午休
         </div>
-        <div class="flex-1 lg:w-[228px]">
-          議程2
-        </div>
-        <div class="flex-1 lg:w-[228px]">
-          議程3
-        </div>
+
+        <!-- 下午議程 -->
+        <NuxtLink
+          v-for="(agenda, index) in agendasAtDec12Afternoon"
+          :key="`${agenda.startTime}-${index}`"
+          to="/agenda/v2"
+          class="col-span-1 lg:h-[228px]"
+        >
+          {{ agenda.topic || agenda.label }}
+        </NuxtLink>
       </div>
     </section>
   </main>
