4b57891765d49142eac31114d698f1c7f13510ef
Shin <akiyachiumi@gmail.com>
2025-10-26 03:24:23 +0800

    refactor: 分離議程篩選側邊欄

---
+++ components/agenda/TagFilterSidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <aside
+    class="hidden shrink-0 flex-col items-start gap-2 border-b border-r border-webconf-gray bg-black p-4 xl:flex xl:w-[228px] xl:pl-4 2xl:w-[260px] 2xl:pl-12"
+  >
+    <AgendaTagFilterBtn size="lg" />
+    <span
+      class="tag-filter-title inline-block text-left text-h4-60 text-webconf-blue"
+    >
+      FILTER
+    </span>
+  </aside>
+</template>
+
+<style scoped>
+.tag-filter-title {
+  writing-mode: vertical-lr;
+  transform: translateX(-4px);
+}
+</style>
