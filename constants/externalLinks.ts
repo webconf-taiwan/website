ec7e43625cd416e3c7d768b7027312561487bad5
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2025-11-08 00:43:20 +0800

    fix: 修改 Banner svg & 新增回顧 webconf 歷史花絮連結

--- constants/externalLinks.ts
+++ constants/externalLinks.ts
@@ -2,4 +2,7 @@ export const EXTERNAL_LINKS = {
   // 購票連結
   CONF_TICKET_URL: 'https://ezbundle.cc/p/webconf-tw-2025',
   WORKSHOP_TICKET_URL: 'https://ezbundle.cc/p/webconf-tw-2025-workshop',
+
+  // 歷屆活動花絮
+  WEBCONF_GALLERY_URL: 'https://gallery.webconf.tw/',
 }
