25089837db570720c503c3c746bd68253e354ae5
Shin <akiyachiumi@gmail.com>
2025-11-10 02:14:25 +0800

    refactor: 調整部分樣式與檔案命名

--- pages/history/v2.vue
+++ pages/history/v2.vue
@@ -5,28 +5,28 @@ useSeoMeta({
   title: '歷屆回顧',
 })
 
-const HISTORY_LIST = [
+const WEBSITES = [
   {
     year: 2024,
     desc: '本屆科技年會將探討 UI/UX 設計的最新趨勢，包括使用者界面設計、使用者體驗優化、人機互動設計等議題，以深入探討如何打造出引人入勝的用戶體驗，提升產品的價值和競爭力。',
     link: 'https://2024.webconf.tw',
-    img: '/images/history-2024.webp',
+    img: '/images/history/webconf-2024.webp',
   },
   {
     year: 2023,
     desc: '重啟十年前的 WebConf 研討會中，帶領與會者穿越時間，探索網路的過去、現在和未來。過去十年間，網路發生了巨大的變化，我們將重新審視網路的起源以及它在這些年裡的演進。',
     link: 'https://2023.webconf.tw',
-    img: '/images/history-2023.webp',
+    img: '/images/history/webconf-2023.webp',
   },
   {
     year: 2013,
     desc: '本屆科技年會將探討 UI/UX 設計的最新趨勢，包括使用者界面設計、使用者體驗優化、人機互動設計等議題，以深入探討如何打造出引人入勝的用戶體驗，提升產品的價值和競爭力。',
     link: 'https://2013.webconf.tw',
-    img: '/images/history-2013.webp',
+    img: '/images/history/webconf-2013.webp',
   },
 ]
 
-const hoveredYear = ref<number | null>(null)
+const hoveredImageIndex = ref(-1)
 </script>
 
 <template>
@@ -117,61 +117,68 @@ const hoveredYear = ref<number | null>(null)
     <main class="lg:mb-40 lg:mt-[100px]">
       <ul class="flex w-full flex-col lg:gap-[100px] lg:px-[calc(100%/9)]">
         <li
-          v-for="(item, index) in HISTORY_LIST"
+          v-for="(item, index) in WEBSITES"
           :key="item.year"
           :class="{
             'lg:self-end': index % 2 === 1,
           }"
           class="hover:blue-shadow group relative border-b border-webconf-gray/50 bg-black text-white duration-300 lg:w-[calc(100dvw/2)]"
-          @mouseenter="hoveredYear = item.year"
-          @mouseleave="hoveredYear = null"
+          @mousemove="hoveredImageIndex = index"
+          @mouseleave="hoveredImageIndex = -1"
         >
-          <NuxtImg
-            :src="item.img"
-            :alt="`${item.year} 網站示意圖`"
-            class="size-full object-cover"
-          />
-          <div
-            class="gap-5 px-5 pb-12 pt-5 duration-300 group-hover:bg-webconf-blue lg:flex 2xl:gap-[68px] 2xl:px-6 2xl:pb-10 2xl:pt-6"
-          >
-            <h2 class="text-h4-60">
-              {{ item.year }}
-            </h2>
-
-            <p class="mt-3 text-body-18 lg:mt-0">
-              {{ item.desc }}
-            </p>
-
-            <NuxtLink
-              :to="item.link"
-              target="_blank"
-              class="mt-6 inline-block bg-webconf-blue px-6 py-2 lg:hidden"
+          <div class="size-full">
+            <NuxtImg
+              :src="item.img"
+              :alt="`${item.year} WebConf 網站封面`"
+              class="size-full object-cover"
+            />
+            <div
+              class="gap-5 px-5 pb-12 pt-5 duration-300 lg:flex group-hover:lg:bg-webconf-blue 2xl:gap-[68px] 2xl:px-6 2xl:pb-10 2xl:pt-6"
             >
-              前往頁面
-            </NuxtLink>
+              <h2 class="text-h4-60">
+                {{ item.year }}
+              </h2>
 
-            <a
-              v-cursor="{
-                scale: 5,
-                duration: 0.5,
-                backgroundColor: 'rgba(0, 46, 255, 0.9)',
-                text: 'VIEW',
-              }"
-              :href="item.link"
-              target="_blank"
-              class="absolute left-0 top-0 mt-6 hidden size-full bg-webconf-blue px-6 py-2 lg:block lg:bg-transparent"
-            >
-            </a>
+              <p class="mt-3 text-body-18 lg:mt-0">
+                {{ item.desc }}
+              </p>
+
+              <NuxtLink
+                :to="item.link"
+                target="_blank"
+                class="mt-6 inline-block bg-webconf-blue px-6 py-2 lg:hidden"
+              >
+                前往頁面
+              </NuxtLink>
+
+              <a
+                v-cursor="{
+                  scale: 5,
+                  duration: 0.5,
+                  backgroundColor: 'rgba(0, 46, 255, 0.9)',
+                  text: 'VIEW',
+                }"
+                :href="item.link"
+                target="_blank"
+                class="absolute left-0 top-0 mt-6 hidden size-full bg-webconf-blue px-6 py-2 lg:block lg:bg-transparent"
+              >
+              </a>
+            </div>
           </div>
-
-          <NuxtImg
-            :key="item.year"
-            :src="item.img"
-            class="pointer-events-none fixed left-0 top-0 z-[-1] hidden h-[100dvh] w-screen object-cover opacity-0 blur-[8px] duration-500 ease-in-out group-hover:opacity-60 lg:block"
-          />
         </li>
       </ul>
     </main>
+
+    <NuxtImg
+      v-for="(item, index) in WEBSITES"
+      :key="item.year"
+      :src="item.img"
+      :alt="`${item.year} WebConf 網站封面`"
+      :class="{
+        'opacity-60': hoveredImageIndex === index,
+      }"
+      class="pointer-events-none fixed left-0 top-0 z-[-1] hidden h-[100dvh] w-screen object-cover opacity-0 blur-[8px] duration-500 ease-in-out group-hover:opacity-60 lg:block"
+    />
 
     <section
       class="z-10 flex h-[500px] flex-col items-center justify-center border-y border-webconf-gray bg-[url('/images/CTABg.webp')] bg-cover bg-center bg-no-repeat"
@@ -179,6 +186,7 @@ const hoveredYear = ref<number | null>(null)
       <h3 class="text-h4-60 text-webconf-gray">
         歷屆活動花絮
       </h3>
+
       <ShareLinkButton
         :to="EXTERNAL_LINKS.WEBCONF_GALLERY_URL"
         target="_blank"
