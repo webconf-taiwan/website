000e474e1b5695f38af3ec64150da0bded0c06ee
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2025-11-12 17:37:32 +0800

    fix: 移除講者內頁舊標籤設定

--- components/content/SpeakerEarnings.vue
+++ components/content/SpeakerEarnings.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="$slots.expectedEarnings"
-    class="custom-ul-style mb-10 flex flex-col gap-4 text-body-16 text-webconf-gray lg:mb-0"
+    class="custom-ul-style flex flex-col gap-4 text-body-16 text-webconf-gray"
   >
     <slot name="expectedEarnings"></slot>
   </div>
