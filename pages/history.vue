ca4ce77f8c368106299891591d09cdb4d7add054
Shin <akiyachiumi@gmail.com>
2025-12-01 00:54:48 +0800

    refactor: 分離靜態資料

--- pages/history.vue
+++ pages/history.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { CONF_LINKS } from '~/constants/confLinks'
 import { EXTERNAL_LINKS } from '~/constants/externalLinks'
 
 useSeoMeta({
@@ -6,27 +7,6 @@ useSeoMeta({
 })
 
 const { gsap } = useGsap()
-
-const WEBSITES = [
-  {
-    year: 2024,
-    desc: '本屆科技年會將探討 UI/UX 設計的最新趨勢，包括使用者界面設計、使用者體驗優化、人機互動設計等議題，以深入探討如何打造出引人入勝的用戶體驗，提升產品的價值和競爭力。',
-    link: 'https://2024.webconf.tw',
-    img: '/images/history/webconf-2024.webp',
-  },
-  {
-    year: 2023,
-    desc: '重啟十年前的 WebConf 研討會中，帶領與會者穿越時間，探索網路的過去、現在和未來。過去十年間，網路發生了巨大的變化，我們將重新審視網路的起源以及它在這些年裡的演進。',
-    link: 'https://2023.webconf.tw',
-    img: '/images/history/webconf-2023.webp',
-  },
-  {
-    year: 2013,
-    desc: '聚焦於網站開發的全方位議題，從前端設計、介面體驗，到後端架構、資料安全與法律規範，全面探索網站建置與經營的多重面向。透過業界講師的實務分享，深入剖析技術與設計的整合應用，帶領與會者掌握 Web 開發的最新趨勢與實踐方法，打造兼具創意與效能的網頁體驗。',
-    link: 'https://2013.webconf.tw',
-    img: '/images/history/webconf-2013.webp',
-  },
-]
 
 const hoveredImageIndex = ref(-1)
 
@@ -45,7 +25,7 @@ function useItemHover(index: number) {
   return itemRef
 }
 
-const itemRefs = WEBSITES.map((_, i) => useItemHover(i))
+const itemRefs = CONF_LINKS.map((_, i) => useItemHover(i))
 
 // 手動觸發 v-cursor 效果
 function useCursorLink() {
@@ -71,7 +51,7 @@ function useCursorLink() {
   return linkRef
 }
 
-const cursorLinkRefs = WEBSITES.map(() => useCursorLink())
+const cursorLinkRefs = CONF_LINKS.map(() => useCursorLink())
 
 const historyBgRef = ref<HTMLDivElement | null>(null)
 
@@ -107,7 +87,7 @@ onMounted(() => {
     <main class="lg:mb-40 lg:mt-[100px]">
       <ul class="flex w-full flex-col lg:gap-[100px] lg:px-[calc(100%/9)]">
         <li
-          v-for="(item, index) in WEBSITES"
+          v-for="(item, index) in CONF_LINKS"
           :key="item.year"
           :ref="
             (el) => {
@@ -170,7 +150,7 @@ onMounted(() => {
     </main>
 
     <NuxtImg
-      v-for="(item, index) in WEBSITES"
+      v-for="(item, index) in CONF_LINKS"
       :key="item.year"
       :src="item.img"
       :alt="`${item.year} WebConf 網站封面`"
