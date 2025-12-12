401915777d7baf6b613cdedb39d6721422fe3add
penspulse326 <penspulse@gmail.com>
2025-12-12 10:39:32 +0800

    perf: 將讀取講者資料的 fetch 抽象成 composable，並在 default layout 中與頁面載入動畫同時執行

--- pages/speakers/[...speakerId].vue
+++ pages/speakers/[...speakerId].vue
@@ -3,8 +3,7 @@ const route = useRoute('speakers-speakerId')
 const router = useRouter()
 const { setToggleModal } = useGlobalState()
 
-const { data: allSpeakers } = await useAsyncData('all-speakers', () =>
-  queryCollection('content').all())
+const { data: allSpeakers } = await useAllSpeakers()
 
 const currentSpeaker = computed(() => {
   const speakerIds = route.params.speakerId
