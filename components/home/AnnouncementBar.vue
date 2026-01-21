9416f28e27e906342b382131a132fdd52c374101
凌俊豪 | Antonio <qd513020@gmail.com>
2026-01-21 10:04:37 +0800

    feat: 新增 webconf 2026 AnnouncementBar (#270)
    Co-authored-by: Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>

---
+++ components/home/AnnouncementBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="fixed inset-x-0 bottom-0 z-[100] flex w-[100dvw] bg-[rgba(0,0,0,0.85)]"
+  >
+    <div
+      class="mx-auto my-3 flex flex-col items-center justify-center font-bold leading-[1.6] md:flex-row md:text-body-18"
+    >
+      <div
+        class="ml-[-18px] w-full text-center text-webconf-gray md:ml-0 md:w-auto md:text-start"
+      >
+        【新頁面即將釋出】
+      </div>
+
+      <div class="text-center text-white">
+        WebConf 2026 即將到來，敬請期待！
+      </div>
+    </div>
+  </div>
+</template>
